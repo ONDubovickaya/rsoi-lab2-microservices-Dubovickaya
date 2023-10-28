5b67dae32ce9b782a15b1315e119ea5f79b6dd7e
ONDubovickaya <144958680+ONDubovickaya@users.noreply.github.com>
2023-10-28 18:31:25 +0300

    Update getCar.py

--- app/services/carsService/cars/interface/getCar.py
+++ app/services/carsService/cars/interface/getCar.py
@@ -4,12 +4,10 @@ from cars.models.modelsClass import CarsModel
 
 getcarb = Blueprint('get_car', __name__, )
 
-
 @getcarb.route('/api/v1/cars/<string:carUid>', methods=['GET'])
 async def get_car(carUid: str) -> Response:
     try:
         car = CarsModel.select().where(CarsModel.car_uid == carUid).get().to_dict()
-
         return Response(status=200, content_type='application/json', response=json.dumps(car))
     except:
         return Response(status=404, content_type='application/json', response=json.dumps({'errors': ['No Uid']}))
