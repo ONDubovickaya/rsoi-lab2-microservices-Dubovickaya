2224660bb94ca2d1921a5130cc5df28f89ba3493
ONDubovickaya <144958680+ONDubovickaya@users.noreply.github.com>
2023-10-28 21:06:32 +0300

    Update deletePayment.py

--- app/services/paymentService/payment/interface/deletePayment.py
+++ app/services/paymentService/payment/interface/deletePayment.py
@@ -2,30 +2,16 @@ import json
 from quart import Blueprint, Response
 from payment.models.modelsClass import PaymentModel
 
-
 deletepaymentb = Blueprint('delete_current_payment', __name__,)
-
 
 @deletepaymentb.route('/api/v1/payment/<string:paymentUid>', methods=['DELETE'])
 async def get_current_payment(paymentUid: str) -> Response:
     try:
-        payment = PaymentModel.select().where(
-            PaymentModel.payment_uid == paymentUid
-        ).get()
+        payment = PaymentModel.select().where(PaymentModel.payment_uid == paymentUid).get()
 
         payment.status = 'CANCELED'
         payment.save()
 
-        return Response(
-            status=200,
-            content_type='application/json',
-            response=json.dumps(payment.to_dict())
-        )
+        return Response(status=200, content_type='application/json', response=json.dumps(payment.to_dict()))
     except:
-        return Response(
-            status=404,
-            content_type='application/json',
-            response=json.dumps({
-                'errors': ['No Uid']
-            })
-        )
+        return Response(status=404, content_type='application/json', response=json.dumps({'errors': ['No Uid']}))
